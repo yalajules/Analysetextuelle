ccb7fe73e5e9911bc686469286b184d50f36cc37
yalajules <yalajules@yahoo.fr>
2019-06-12 13:11:18 +0200

    Update analysetexte.py

--- analysetexte.py
+++ analysetexte.py
@@ -4,17 +4,23 @@ def traitementtextuel (iter):
         Crée un dictionnaire (clé=item de l'itérable ; valeur=liste(nboccurences;fréquence)
         Affiche par ordre décroissant d'occurences les items du dictonnaire convertit préalablement en liste
     """
-    longueur=len(iter)
+    longmaxitem=0
+    for k in iter :
+        if len(k) > longmaxitem :
+            longmaxitem=len(k)
+
+    longueuriter=len(iter)
+    
     dicoiters={}
     for char in iter : #dico[0]=nb occurences ; dico[1]=fréquences
         dicoiters[char]=[0,0]
     for char in iter :
         dicoiters[char][0]=dicoiters[char][0]+1
-        dicoiters[char][1]=dicoiters[char][0]/longueur*100
+        dicoiters[char][1]=dicoiters[char][0]/longueuriter*100
     listetriee= sorted(dicoiters.items(), key=lambda x: x[1][0], reverse=True)
 
     for tuple in listetriee :
-        print(tuple[0],': Nombre d\'occurences =',tuple[1][0],'et fréquence = ',tuple[1][1])
+        print(f"{tuple[0]:^3}  Nombre d\'occurences = {tuple[1][0]} et fréquence = {tuple[1][1]:>.2f} %")
 
 def traitementmots (chaine):
     """
