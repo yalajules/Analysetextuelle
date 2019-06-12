1dc1a07064af300aa949b97007724d0f5af62d11
jfpaget <51720126+jfpaget@users.noreply.github.com>
2019-06-12 15:10:18 +0200

    Update lecture_fichier.py

--- lecture_fichier.py
+++ lecture_fichier.py
@@ -25,30 +25,6 @@ def lecture_fichier() :
         texte = texte[:len(texte)-1]
     return texte, nb_lignes
 
-def formatage_mots(chaine):
-    chaine0 = chaine[0]
-
-    # Les caractères de ponctuation sont remplacés par des espaces
-    ponctuation = ["'",'"',",",";",".","!","?","{","}","[","]","(",")"]
-    for car in chaine[1:]:
-        if car not in ponctuation :
-            chaine0 += car
-        else :
-            chaine0 += " "
-    # Les espaces qui se suivent sont remplacés par un seul espace
-    chaine1 = chaine0.replace("  "," ")
-    # Suppression des doubles espaces
-    while len(chaine1) != len(chaine0):
-        chaine0 = chaine1
-        chaine1 = chaine0.replace("  "," ")
-       
-    # Une liste de mot est générée sous forme de liste
-    liste_mots = chaine1.split(" ")
-    # Si le dernier élément de la liste est vide
-    if liste_mots[len(liste_mots)-1] == '':
-        liste_mots.remove("")
-    return liste_mots
-
 texte, lignes = lecture_fichier()
 mots = formatage_mots(texte)
 nombre = len(mots)
