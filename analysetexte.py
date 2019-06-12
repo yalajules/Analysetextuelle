3bcf99908d881df8e2b5498a8d4b27be3b519cdf
yalajules <yalajules@yahoo.fr>
2019-06-12 12:14:56 +0200

    Minor changes

--- analysetexte.py
+++ analysetexte.py
@@ -1,25 +1,25 @@
-def traitementtextuel (objet):
+def traitementtextuel (iter):
     """
         Reçoit un itérable
         Crée un dictionnaire (clé=item de l'itérable ; valeur=liste(nboccurences;fréquence)
-        Affiche par ordre décroissant d'occurences les objets du dicitonnaire convertit préalablement en liste
+        Affiche par ordre décroissant d'occurences les items du dictonnaire convertit préalablement en liste
     """
-    longueur=len(objet)
-    dicoobjets={}
-    for char in objet : #dico[0]=nb occurences ; dico[1]=fréquences
-        dicoobjets[char]=[0,0]
-    for char in objet :
-        dicoobjets[char][0]=dicoobjets[char][0]+1
-        dicoobjets[char][1]=dicoobjets[char][0]/longueur*100
-    listetriee= sorted(dicoobjets.items(), key=lambda x: x[1][0], reverse=True)
+    longueur=len(iter)
+    dicoiters={}
+    for char in iter : #dico[0]=nb occurences ; dico[1]=fréquences
+        dicoiters[char]=[0,0]
+    for char in iter :
+        dicoiters[char][0]=dicoiters[char][0]+1
+        dicoiters[char][1]=dicoiters[char][0]/longueur*100
+    listetriee= sorted(dicoiters.items(), key=lambda x: x[1][0], reverse=True)
 
     for tuple in listetriee :
         print(tuple[0],': Nombre d\'occurences =',tuple[1][0],'et fréquence = ',tuple[1][1])
 
-def traitementmots (texte):
+def traitementmots (chaine):
     """
         Reçoit un texte
-        Appel à traitementtextuel
+        Appel à la fonction traitementtextuel
     """
     listemots=texte.split(' ')
     traitementtextuel(listemots)
